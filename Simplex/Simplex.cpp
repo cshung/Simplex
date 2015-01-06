74521d1203ef20fa543f46181dd7a9446ec5fc63
cshung <cshung@gmail.com>
2015-01-06 13:05:52 -0800

    Two phase simplex algorithm

--- Simplex/Simplex.cpp
+++ Simplex/Simplex.cpp
@@ -11,20 +11,29 @@ using namespace std;
 
 // Simplex lab with C++
 
-void optimize_tableau(vector<vector<double>>& tableau, vector<int>& basic_columns);
-
+/* public */
 void linear_programming(vector<double> objective_vector, vector<vector<double>> constraint_matrix, vector<double> constraint_vector);
+
+/* private */
+void optimize_tableau(vector<vector<double>>& tableau, vector<int>& basic_columns, int phase);
 
 int main()
 {
     // Initialize tableau with sample data from book, sample problem comes from
     // Combinatorial Optimization - Algorithms and Complexity (Chapter 2 - example 2.5)
 
-    string line1 = "  3   5";
-    string line2 = "      1   1   1   1   1";
+    /*string line1 = "  3   5                ";
+    string line2 = "  1   1   1   1   1    ";
     string line3 = "  3   2   1   0   0   1";
     string line4 = "  5   1   1   1   0   3";
-    string line5 = "  2   5   1   0   1   4";
+    string line5 = "  2   5   1   0   1   4";*/
+
+    string line1 = "  3   5                ";
+    string line2 = "     -1  -2   0   0   0";
+    string line3 = " -2   1   1   0   0   2";
+    string line4 = " -1   1   0   1   0   3";
+    string line5 = "  1   0   0   0   1   3";
+
     string endline = "\r\n";
     istringstream iss(line1 + endline + line2 + endline + line3 + endline + line4 + endline + line5);
 
@@ -50,6 +59,7 @@ int main()
     {
         iss >> objective_vector[n];
     }
+
     for (int m = 0; m < number_of_constraints; m++)
     {
         for (int n = 0; n < number_of_variables; n++)
@@ -68,17 +78,29 @@ void linear_programming(vector<double> objective_vector, vector<vector<double>> 
 {
     // Step 1: Initialize tableau (for phase 1 to determine feasible basis)
     int number_of_constraints = constraint_matrix.size();
-    int number_of_variables = objective_vector.size() + number_of_constraints;
+    int number_of_original_variables = objective_vector.size();
+    int number_of_variables = number_of_original_variables + number_of_constraints;
     vector<vector<double>> tableau;
-    tableau.resize(number_of_constraints + 1);
-    for (int m = 0; m < number_of_constraints + 1; m++)
+    tableau.resize(number_of_constraints + 2);
+    for (int m = 0; m < number_of_constraints + 2; m++)
     {
         tableau[m].resize(number_of_variables + 1);
         for (int n = 0; n <= number_of_variables; n++)
         {
-            if (m == 0)
+            if (m == 0) // original objective row
             {
-                if (1 <= n && n <= number_of_constraints)
+                if (1 <= n && n <= number_of_original_variables)
+                {
+                    tableau[m][n] = objective_vector[n - 1];
+                }
+                else
+                {
+                    tableau[m][n] = 0;
+                }
+            }
+            else if (m == number_of_constraints + 1)
+            {
+                if (n > number_of_original_variables)
                 {
                     tableau[m][n] = 1;
                 }
@@ -93,47 +115,55 @@ void linear_programming(vector<double> objective_vector, vector<vector<double>> 
                 {
                     tableau[m][n] = constraint_vector[m - 1];
                 }
-                else if (1 <= n && n <= number_of_constraints)
+                else if (n > number_of_original_variables)
                 {
-                    tableau[m][n] = (m == n) ? 1 : 0;
+                    tableau[m][n] = (m == n - number_of_original_variables) ? 1 : 0;
                 }
                 else
                 {
-                    tableau[m][n] = constraint_matrix[m - 1][n - number_of_constraints - 1];
+                    tableau[m][n] = constraint_matrix[m - 1][n - 1];
                 }
             }
         }
     }
 
     // Step 2: Initialize relative costs
-    for (int m = 1; m < number_of_constraints + 1; m++)
+    for (int m = 1; m <= number_of_constraints; m++)
     {
         for (int n = 0; n <= number_of_variables; n++)
         {
-            tableau[0][n] -= tableau[m][n];
+            tableau[number_of_constraints + 1][n] -= tableau[m][n];
         }
     }
 
     // Step 3: Initialize the set of basic columns
     vector<int> basic_columns;
-    basic_columns.push_back(1);
-    basic_columns.push_back(2);
-    basic_columns.push_back(3);
+    for (int m = 1; m <= number_of_constraints; m++)
+    {
+        basic_columns.push_back(m + number_of_original_variables);
+    }
 
-    optimize_tableau(tableau, basic_columns);
+    optimize_tableau(tableau, basic_columns, /* phase = */1);
+    optimize_tableau(tableau, basic_columns, /* phase = */2);
 }
 
-void optimize_tableau(vector<vector<double>>& tableau, vector<int>& basic_columns)
+void optimize_tableau(vector<vector<double>>& tableau, vector<int>& basic_columns, int phase)
 {
-    int number_of_constraints = tableau.size() - 1;
+    int number_of_constraints = tableau.size() - 2;
     int number_of_variables = tableau[0].size() - 1;
+    if (phase == 2)
+    {
+        number_of_variables -= number_of_constraints;
+    }
+
+    int objective_row = phase == 1 ? number_of_constraints + 1 : 0;
     while (true) /* optimal will break */
     {
         // Step 3: Initialize the column_to_enter_basis (bland's algorithm - first negative cost column enter basis)
         int column_to_enter_basis = -1;
         for (int c = 1; c <= number_of_variables; c++)
         {
-            if (tableau[0][c] < 0)
+            if (tableau[objective_row][c] < 0)
             {
                 column_to_enter_basis = c;
                 break;
@@ -179,7 +209,7 @@ void optimize_tableau(vector<vector<double>>& tableau, vector<int>& basic_column
         }
 
         // Step 6: Zero out the other rows
-        for (int other_row = 0; other_row < (number_of_constraints + 1); other_row++)
+        for (int other_row = 0; other_row < (number_of_constraints + 2); other_row++)
         {
             if (other_row != row)
             {
@@ -192,7 +222,7 @@ void optimize_tableau(vector<vector<double>>& tableau, vector<int>& basic_column
         }
 
         // Show the tableau
-        for (int m = 0; m < (number_of_constraints + 1); m++)
+        for (int m = 0; m < (number_of_constraints + 2); m++)
         {
             for (int n = 0; n < (number_of_variables + 1); n++)
             {
